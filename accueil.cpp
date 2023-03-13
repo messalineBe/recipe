29cc40794690d4fec9404b3df77cbdd1934a7eb1
messaline <messa@b.com>
2023-03-13 14:56:04 +0100

    ajout boutons pour dossiers

--- accueil.cpp
+++ accueil.cpp
@@ -5,6 +5,24 @@ Accueil::Accueil(QWidget *parent)
     : QWidget(parent){
         //taille de la fenêtre
         setFixedSize(1500,800);
-        m_button = new QPushButton("Hello", this);
-        m_button -> setGeometry(10, 10, 300, 300);
+
+        //Barre de recherceh
+        QLineEdit *recherche = new QLineEdit();
+        recherche->setPlaceholderText("Ex: Qt");
+
+        //pour le setGeometry(positionAbsc, positionOrdo, longueur,hauteur)
+
+        //bouton ajouter
+        buttonAddRecipe = new QPushButton("Ajouter une recette", this);
+        buttonAddRecipe -> setGeometry(700, 300, 250, 70);
+
+        //boutons dossiers
+        buttonEntreeFolder = new QPushButton("Entrée", this);
+        buttonEntreeFolder -> setGeometry(20, 200, 250, 70);
+
+        buttonPlatsFolder = new QPushButton("Plats", this);
+        buttonPlatsFolder -> setGeometry(300, 200, 250, 70);
+
+        buttonDessertsFolder = new QPushButton("Desserts", this);
+        buttonDessertsFolder -> setGeometry(580, 200, 250, 70);
     }
--- accueil.hpp
+++ accueil.hpp
@@ -1,12 +1,17 @@
 // #ifndef FENETRE_HPP
 // #define FENETRE_HPP
 #include <QWidget>
+#include <QLineEdit>
 
 class QPushButton;
 class Accueil : public QWidget{
     public:
         explicit Accueil(QWidget *parent = nullptr);
     private:
-        QPushButton * m_button;
+        QPushButton * buttonAddRecipe;
+        QPushButton * buttonEntreeFolder;
+        QPushButton * buttonPlatsFolder;
+        QPushButton * buttonDessertsFolder;
+        QLineEdit * recherche;
 };
 // #endif
