cffea9b41da9b0f2e9492d761533f557d00837ac
messaline <messa@b.com>
2023-03-22 09:55:47 +0100

    delete

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "accueil.hpp"
-
-int main (int argc, char * argv[]){
-    QApplication app(argc, argv);
-    Accueil window;
-    window.show();
-    return app.exec();
-}
--- recipe.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = app
-TARGET = recipe
-QT = core gui widgets
-SOURCES += main.cpp accueil.cpp editRecipe.cpp
-HEADERS += accueil.hpp
